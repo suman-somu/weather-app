7bfca5e960d2020cedd8a5f5f0660ffb645e9271
Suman Sahoo <ssamsomu@gmail.com>
2022-09-11 18:40:53 +0530

    got user location
    Got the user location at the beginning and showed it
    tasks - add the functionality to update the output according to any number of times according to user pressing to refresh button

--- app/src/main/java/com/example/weatherapp/MainActivity.kt
+++ app/src/main/java/com/example/weatherapp/MainActivity.kt
@@ -1,28 +1,80 @@
 package com.example.weatherapp
 
 import android.Manifest
-import android.content.pm.PackageManager
 import android.location.Location
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.TextView
 import androidx.activity.result.contract.ActivityResultContracts
-import androidx.core.app.ActivityCompat
+import androidx.appcompat.app.AppCompatActivity
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationServices
-import com.google.android.gms.tasks.Task
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var fusedLocationClient: FusedLocationProviderClient
+
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
 
+        val locationPermissionRequest = registerForActivityResult(
+            ActivityResultContracts.RequestMultiplePermissions()
+        ) { permissions ->
+            when {
+                permissions.getOrDefault(Manifest.permission.ACCESS_FINE_LOCATION, false) -> {
+                    // Precise location access granted.
+                }
+                permissions.getOrDefault(Manifest.permission.ACCESS_COARSE_LOCATION, false) -> {
+                    // Only approximate location access granted.
+                } else -> {
+                // No location access granted.
+            }
+            }
+        }
+        locationPermissionRequest.launch(arrayOf(
+            Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.ACCESS_COARSE_LOCATION))
+
+
+
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+
+        fusedLocationClient.lastLocation
+            .addOnSuccessListener { location : Location? ->
+                // Got last known location. In some rare situations this can be null.
+
+                var longitude = location?.longitude
+                var latitude = location?.latitude
+
+                Log.d("RIYA ","$longitude $latitude")
+
+
+                var lngtd = findViewById<View>(R.id.temp_max) as TextView
+                var lttd = findViewById<View>(R.id.temp_min) as TextView
+
+                var l1 = longitude?.toBigDecimal()?.toPlainString()
+                lngtd.setText("longitude = $l1")
+                lngtd.invalidate()
+                lngtd.requestLayout()
+
+                var l2 = latitude?.toBigDecimal()?.toPlainString()
+                lttd.setText("lattitude = $l2")
+                lttd.invalidate()
+                lttd.requestLayout()
+
+
+                //lttd.setText(latitude)
+
+            }
+
 
     }
+
 }
 
