df16ec64c8434cffdb65d591b32ac5ee9e016bc1
Suman Sahoo <ssamsomu@gmail.com>
2022-09-11 16:51:02 +0530

    basic ui of app

---
+++ app/src/main/java/com/example/weatherapp/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.weatherapp
+
+import android.Manifest
+import android.content.pm.PackageManager
+import android.location.Location
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.tasks.Task
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+
+    }
+}
+
