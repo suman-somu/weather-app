e75c59ff5d4d4839993f9eb4a209f41dab60a170
Suman Sahoo <ssamsomu@gmail.com>
2022-09-15 21:44:06 +0530

    no chnge

--- app/src/main/java/com/example/weatherapp/MainActivity.kt
+++ app/src/main/java/com/example/weatherapp/MainActivity.kt
@@ -4,6 +4,7 @@ import android.Manifest
 import android.content.pm.PackageManager
 import android.location.Location
 import android.os.Bundle
+import android.os.Looper
 import android.util.Log
 import android.view.View
 import android.widget.Button
@@ -13,11 +14,13 @@ import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationResult
 import com.google.android.gms.location.LocationServices
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var fusedLocationClient: FusedLocationProviderClient
+    private lateinit var locationCallback: LocationCallback
 
 
 
@@ -44,9 +47,6 @@ class MainActivity : AppCompatActivity() {
         ) { permissions ->
             when {
                 permissions.getOrDefault(Manifest.permission.ACCESS_FINE_LOCATION, false) -> {
-                    // Precise location access granted.
-                }
-                permissions.getOrDefault(Manifest.permission.ACCESS_COARSE_LOCATION, false) -> {
                     // Only approximate location access granted.
                 } else -> {
                 // No location access granted.
@@ -54,13 +54,13 @@ class MainActivity : AppCompatActivity() {
             }
         }
         locationPermissionRequest.launch(arrayOf(
-            Manifest.permission.ACCESS_FINE_LOCATION,
-            Manifest.permission.ACCESS_COARSE_LOCATION))
+            Manifest.permission.ACCESS_FINE_LOCATION))
 
 
         var getweather = findViewById<View>(R.id.getweather) as Button
 
         getweather.setOnClickListener {
+
             val text = "button cliked!"
             val duration = Toast.LENGTH_SHORT
 
@@ -70,15 +70,13 @@ class MainActivity : AppCompatActivity() {
 
 
 
+            var fusedLocationClient: FusedLocationProviderClient
 
             fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
 
-            if (ActivityCompat.checkSelfPermission(
+            if ( ActivityCompat.checkSelfPermission(
                     this,
-                    Manifest.permission.ACCESS_FINE_LOCATION
-                ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
-                    this,
-                    Manifest.permission.ACCESS_COARSE_LOCATION
+                        Manifest.permission.ACCESS_FINE_LOCATION
                 ) != PackageManager.PERMISSION_GRANTED
             ) {
                 // TODO: Consider calling
@@ -97,9 +95,6 @@ class MainActivity : AppCompatActivity() {
                     var longitude = location?.longitude
                     var latitude = location?.latitude
 
-                    Log.d("RIYA ","$longitude $latitude")
-
-
                     var lngtd = findViewById<View>(R.id.temp_max) as TextView
                     var lttd = findViewById<View>(R.id.temp_min) as TextView
 
@@ -117,7 +112,10 @@ class MainActivity : AppCompatActivity() {
 
                 }
 
+            fusedLocationClient.
+
         }
+
     }
 }
 
